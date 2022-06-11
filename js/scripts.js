112d39ff5d1582ff9c312cae5a9d1524846339b0
mejiabrayan <learnwithbrayan@gmail.com>
2022-06-11 14:05:00 -0700

    Added objects to js/script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-let favoriteFruit = 'Watermelon';
-document.write(favoriteFruit);
-alert('Hello World');
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    abilities: ['Chlorophyll', 'Overgrow'],
+    types: ["grass", "poison"],
+  },
+  {
+      name: 'Charmander',
+      height: 0.6,
+      abilities: ['Blaze', 'Solar-power'],
+      types: ['Fire']
+  },
+  {
+      name: 'Squirtle',
+      height: 0.5,
+      abilities: ['Rain-dish', 'Torrent'],
+      types: ['Water']
+  }
+];
+
+console.log(pokemonList);
+
