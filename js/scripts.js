3a2d8fad074d3280f84aac85c69ed5e7cd42e6f5
mejiabrayan <learnwithbrayan@gmail.com>
2022-06-13 15:49:50 -0700

    Added objects & Loops to js/scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,51 @@
 let pokemonList = [
   {
-    name: 'Bulbasaur',
+    name: "Bulbasaur",
     height: 0.7,
-    abilities: ['Chlorophyll', 'Overgrow'],
-    types: ["grass", "poison"],
+    weight: "15.2 lbs",
+    abilities: ["Chlorophyll", "Overgrow"],
+    category: "Seed",
+    types: ["Grass", "Poison"],
+    weakness: ["Fire", "Pyschic", "Flying", "Ice"],
   },
   {
-      name: 'Charmander',
-      height: 0.6,
-      abilities: ['Blaze', 'Solar-power'],
-      types: ['Fire']
+    name: "Charmander",
+    height: 0.6,
+    weight: "18.7 lbs",
+    abilities: ["Blaze", "Solar-power"],
+    category: "Lizard",
+    types: ["Fire"],
+    weakness: ["Water", "Ground", "Rock"],
   },
   {
-      name: 'Squirtle',
-      height: 0.5,
-      abilities: ['Rain-dish', 'Torrent'],
-      types: ['Water']
-  }
+    name: "Squirtle",
+    height: 0.5,
+    weight: "19.8 lbs",
+    abilities: ["Rain-dish", "Torrent"],
+    category: "Tiny Turtle",
+    types: ["Water"],
+    weakness: ["Grass", "Electric"],
+  },
+  {
+    name: "Arbok",
+    height: "11' 06",
+    weight: "123 lbs",
+    abilities: ["Shred Skin", "Intimidate"],
+    category: "Cobra",
+    types: ["Poison"],
+    weakness: ["Pyschic", "Ground"],
+  },
 ];
 
-console.log(pokemonList);
-
+// iterates over each object
+for (let i = 0; pokemonList.length < 10; i++) {
+  if (pokemonList[i].height < 1.0) {
+    document.write(
+      `${pokemonList[i].name} (height: ${pokemonList[i].height}) - is a small pokemon `
+    );
+  } else {
+    document.write(
+      ` ${pokemonList[i].name} (height ${pokemonList[i].height}) - is a big pokemon! `
+    );
+  }
+}
